6c34b5481fcc395b7217769535a52ecf5a323d67
Sharmanmed <166248641+Sharmanmed@users.noreply.github.com>
2025-02-27 14:05:30 +0500

    Add files via upload

---
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    private static List<TodoItem> _todoItems = new List<TodoItem>();
+    private static int _nextId = 1;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TodoItem>> GetAll()
+    {
+        return Ok(_todoItems);
+    }
+
+    [HttpPost]
+    public ActionResult<TodoItem> Create(TodoItem item)
+    {
+        item.Id = _nextId++;
+        _todoItems.Add(item);
+        return CreatedAtAction(nameof(GetAll), new { id = item.Id }, item);
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var item = _todoItems.FirstOrDefault(x => x.Id == id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        _todoItems.Remove(item);
+        return NoContent();
+    }
+}
---
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }            // Уникальный идентификатор задачи
+        public string Title { get; set; }      // Название задачи
+        public bool IsCompleted { get; set; }  // Статус завершения задачи
+    }
+}
